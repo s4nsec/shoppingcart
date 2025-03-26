3a1c993d5d5ed865314a1a2fbc8f4f4c48bdbeba
s4nsec <s4nsec@gmail.com>
2025-03-26 18:29:09 -0400

    Modify Cart.java
    - Add more formal checks
    - Make other class subclasses of Cart class

--- Cart.java
+++ Cart.java
@@ -1,50 +1,63 @@
 import java.util.HashMap;
 
-class Item{
-    private Integer itemId;
-    private Integer quantity;
-
-    public Item(Integer itemId, Integer quantity){
-        this.itemId = itemId;
-        this.quantity = quantity;
-    }
-
-    public Integer getQuantity(){
-        return this.quantity;
-    }
-
-    public Integer getItemId(){
-        return this.itemId;
-    }
-
-}
-
-class ShoppingCart{
-    private Integer userId;
-    private HashMap<Integer, Integer> items;
-
-    public ShoppingCart(Integer userId){
-        this.userId = userId;
-        this.items = new HashMap<Integer, Integer>();
-    }
-
-    //@ requires itemId >= 0;
-    //@ requires quantity >= 0;
-    public void addItem(Integer itemId, Integer quantity){
-        this.items.put(itemId, quantity);
-    }
-    public void removeItem(){
-        // TODO
-    }
-
-}
-
 public class Cart{
+    static class Item{
+        private Integer itemId;
+        private Integer quantity;
+
+        public Item(Integer itemId, Integer quantity){
+            this.itemId = itemId;
+            this.quantity = quantity;
+        }
+
+        public Integer getQuantity(){
+            return this.quantity;
+        }
+
+        public Integer getItemId(){
+            return this.itemId;
+        }
+
+    }
+
+    static class ShoppingCart{
+        //@ spec_public
+        private Integer _userId;
+        //@ spec_public
+        private HashMap<Integer, Integer> _items;
+
+        /*@
+        @ requires userId != null;
+        @ ensures this._userId == userId;
+        @ ensures this._items != null;
+        @*/
+        public ShoppingCart(Integer userId){
+            this._userId = userId;
+            this._items = new HashMap<Integer, Integer>();
+        }
+        /*@
+        @ requires itemId != null;
+        @ requires quantity != null;
+        @ requires itemId >= 0;
+        @ requires quantity >= 0;
+        @
+        // @ ensures _items.containsKey(itemId);
+        // @ ensures _items.get(itemId) == quantity;
+        @*/
+        public void addItem(Integer itemId, Integer quantity){
+            this._items.put(itemId, quantity);
+        }
+        public void removeItem(){
+            // TODO
+        }
+
+    }
+
     public static void main(String[] args){
         Integer userId = 1;
         ShoppingCart shoppingCart = new ShoppingCart(userId);
         Item item2 = new Item(2, 4);
-        Integer item2ItemId = -2;
+        Integer item2ItemId = 3;
         Integer item2Quantity = 4;
         shoppingCart.addItem(item2ItemId, item2Quantity);
     }
