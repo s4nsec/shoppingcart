4b9c1f0b26e871fd06a538b4f1ff8f4432a0a413
s4nsec <s4nsec@gmail.com>
2025-03-26 22:28:32 -0400

    Add some more specs

--- Cart.java
+++ Cart.java
@@ -2,18 +2,29 @@ import java.util.HashMap;
 
 public class Cart{
     static class Item{
+        //@ spec_public
         private Integer itemId;
+        //@ spec_public
         private Integer quantity;
 
+        //@ public invariant itemId >=0;
+        //@ public invariant quantity >=0;
+
+        //@ requires itemId != null;
+        //@ requires itemId >= 0;
+        //@ requires quantity != null;
+        //@ requires quantity >= 0;
         public Item(Integer itemId, Integer quantity){
             this.itemId = itemId;
             this.quantity = quantity;
         }
 
+        //@ ensures \result == this.quantity;
         public Integer getQuantity(){
             return this.quantity;
         }
 
+        //@ ensures \result == this.itemId;
         public Integer getItemId(){
             return this.itemId;
         }
@@ -26,30 +37,25 @@ public class Cart{
         //@ spec_public
         private HashMap<Integer, Integer> _items;
 
-        /*@
-        @ requires userId != null;
-        @ ensures this._userId == userId;
-        @ ensures this._items != null;
-        @*/
+        //@ private invariant _userId >= 0;
+
+        //@ requires userId != null;
+        //@ ensures this._userId == userId;
+        //@ ensures this._items != null;
         public ShoppingCart(Integer userId){
             this._userId = userId;
             this._items = new HashMap<Integer, Integer>();
         }
-        /*@
-        @ requires itemId != null;
-        @ requires quantity != null;
-        @ requires itemId >= 0;
-        @ requires quantity >= 0;
-        @
-        // @ ensures _items.containsKey(itemId);
-        // @ ensures _items.get(itemId) == quantity;
-        @*/
+
+        //@ requires itemId != null;
+        //@ requires quantity != null;
+        //@ requires itemId >= 0;
+        //@ requires quantity >= 0;
         public void addItem(Integer itemId, Integer quantity){
             this._items.put(itemId, quantity);
         }
-        /*@
-        @ requires itemId != null;
-        @*/
+
+        //@ requires itemId != null;
         public void removeItem(Integer itemId){
             this._items.remove(itemId);
         }
